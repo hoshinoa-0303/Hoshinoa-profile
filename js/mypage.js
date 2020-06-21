2eef9f145984ba0e7fef97ad9134becd05e0c9b9
hoshinoa-0303 <59353950+hoshinoa-0303@users.noreply.github.com>
2020-06-21 22:37:46 +0800

    Add files via upload

---
+++ js/mypage.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('#fullpage').fullpage({
+        navigation: true,
+        scrollingSpeed: 500,
+        resize: true,
+        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fivePage', 'sixthPage', 'seventhPage', 'eighthPage', 'ninthPage', 'lastPage'],
+        loopBottom: true,
+        css3: true,
+    });
+});
